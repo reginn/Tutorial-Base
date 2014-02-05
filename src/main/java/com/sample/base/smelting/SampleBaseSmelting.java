c390babe6d1de3c25d2de6be50e5bb21e784eaac
reginn666 <reginn666@gmail.com>
2014-02-05 21:14:04 +0900

    メタデータ付きの精錬レシピ追加をGameRegistryのメソッドに変更

--- src/main/java/com/sample/base/smelting/SampleBaseSmelting.java
+++ src/main/java/com/sample/base/smelting/SampleBaseSmelting.java
@@ -18,19 +18,16 @@ public class SampleBaseSmelting
 	public void init(FMLInitializationEvent event)
 	{
 		/*
-		 * 土を精錬するとダイアモンドになる精錬レシピを追加する.
-		 * 引数は(精錬するブロック, 精錬結果のItemStack, 精錬後に入手できる経験値)
-		 * アイテムとブロックの場合の2つのメソッドが用意された.
+		 * 土を精錬するとダイアモンドに,
+		 * ダイアモンドを精錬すると土に,
+		 * 黒い羊毛を精錬すると白い羊毛する精錬レシピを追加する.
+		 * 引数は
+		 * (精錬するBlock,     精錬結果のItemStack, 精錬後に入手できる経験値) : Blockの場合
+		 * (精錬するItem,      精錬結果のItemStack, 精錬後に入手できる経験値) : Itemの場合
+		 * (精錬するItemStack, 精錬結果のItemStack, 精錬後に入手できる経験値) : メタデータ付きのBlock/Itemの場合
 		 */
 		GameRegistry.addSmelting(Blocks.dirt,   new ItemStack(Items.diamond), 0.5F);
 		GameRegistry.addSmelting(Items.diamond, new ItemStack(Items.coal),    0.5F);
-
-		/*
-		 * メタデータを持つブロック/アイテムの精錬レシピを追加するAPIがFMLに用意されていない(Forge#985時点).
-		 * そのためFurnaceRecipesを直接呼び出して精錬レシピを追加する.
-		 * 引数は(精錬するブロック/アイテムのItemStack, 精錬結果のItemStack, 精錬後に入手できる経験値)
-		 * 今回は黒い羊毛ブロックを白い羊毛ブロックに精錬するレシピを追加している.
-		 */
-		FurnaceRecipes.smelting().func_151394_a(new ItemStack(Blocks.wool, 1, 15), new ItemStack(Blocks.wool, 1, 0), 0.5F);
+        GameRegistry.addSmelting(new ItemStack(Blocks.wool, 1, 15), new ItemStack(Blocks.wool, 1, 0), 0.5F);
 	}
 }
